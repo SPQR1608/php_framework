58fbb75e00fae256bb183eb8f965a491b0075ee0
SPQR1608 <dimondiulger@mail.ru>
2021-03-26 16:28:53 +0300

    Add Cache class

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,23 +2,30 @@
 namespace app\controllers;
 
 use app\models\Main;
+use vendor\core\App;
 
 class MainController extends AppController
 {
     //public $layout = 'main';
     public function indexAction()
     {
+        //App::$app->getList();
         //$this->layout = false;
         //$this->view = 'test';
 
         $model = new Main;
-
+        \R::fancyDebug(true);
         //$posts = $model->findAll();
         //$post = $model->findOne(2, 'category_id');
         //$posts = $model->findBySql("SELECT * FROM {$model->table} WHERE title LIKE ?", ['%то%']);
         //$posts = $model->findLike('те', 'title');
 
-        $posts = \R::findAll('posts');
+        $posts = App::$app->cache->get('posts');
+        if (!$posts) {
+            $posts = \R::findAll('posts');
+            App::$app->cache->set('posts', $posts);
+        }
+
         $menu = $this->menu;
 
         $this->setMeta('Main Page', 'Description', 'Keywords');
--- vendor/libs/Cache.php
+++ vendor/libs/Cache.php
@@ -6,5 +6,56 @@ namespace vendor\libs;
 
 class Cache
 {
+    public function __construct()
+    {
 
+    }
+
+    /**
+     * @param $key
+     * @param $data
+     * @param int $seconds
+     * @return bool
+     */
+    public function set($key, $data, $seconds = 3600)
+    {
+        $content['data'] = $data;
+        $content['end_time'] = time() + $seconds;
+
+        if (!is_dir(CACHE)) {
+            mkdir(CACHE, 0755, true);
+        }
+
+        if (file_put_contents(CACHE . '/'. md5($key). '.txt', serialize($content))) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param $key
+     * @return false|mixed
+     */
+    public function get($key)
+    {
+        $file = CACHE . '/'. md5($key). '.txt';
+        if (file_exists($file)) {
+            $content = unserialize(file_get_contents($file));
+            if (time() <= $content['end_time']) {
+                return $content['data'];
+            }
+            unlink($file);
+        }
+        return false;
+    }
+
+    /**
+     * @param $key
+     */
+    public function delete($key) {
+        $file = CACHE . '/'. md5($key). '.txt';
+        if (file_exists($file)) {
+            unlink($file);
+        }
+    }
 }
